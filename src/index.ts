c2ed8620a59b38b68b00170856ac79d39fc57bef
Bruce MacDonald <brucewmacdonald@gmail.com>
2024-01-24 13:42:39 -0800

    export types from main entry point

--- src/index.ts
+++ src/index.ts
@@ -313,3 +313,6 @@ export class Ollama {
 }
 
 export default new Ollama()
+
+// export all types from the main entry point so that packages importing types dont need to specify paths
+export * from './interfaces'
