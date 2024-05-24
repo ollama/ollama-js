ccd68cf2c928336ac640abf93c619ba12c390f59
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-05-24 07:21:44 +0200

    Revert "chore: refactoring"
    This reverts commit 8ac2d83ac6fbbcf3a25186fed4ca68a51508836f.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,13 +1,4 @@
 const EMPTY_STRING = ''
-const CODE_404 = '404'
-const PROTOCOLS = {
-  HTTP: 'http',
-  HTTPS: 'https',
-} as const
-const PORTS = {
-  HTTP: '80',
-  HTTPS: '443',
-} as const
 const MESSAGES = {
   MISSING_BODY: 'Missing body',
   SUCCESS: 'Success',
@@ -15,19 +6,11 @@ const MESSAGES = {
   ERROR_FETCHING_TEXT: 'Failed to get text from error response',
   ERROR_NO_MODEL_FILE: 'Must provide either path or modelfile to create a model',
   ERROR_JSON_PARSE: 'Failed to parse error response as JSON',
-  STREAMING_UPLOADS_NOT_SUPPORTED:
-    'Streaming uploads are not supported in this environment.',
 } as const
 const REQUEST_CONSTANTS = {
   GENERATE: 'generate',
   CREATE: 'create',
   PUSH: 'push',
-  PULL: 'pull',
-} as const
-const STREAMING_EVENTS = {
-  DATA: 'data',
-  END: 'end',
-  ERROR: 'error',
 } as const
 const MODEL_FILE_COMMANDS = ['FROM', 'ADAPTER']
 const OLLAMA_LOCAL_URL = 'http://127.0.0.1:11434'
@@ -39,12 +22,8 @@ const ENCODING = {
 } as const
 export {
   EMPTY_STRING,
-  CODE_404,
-  PROTOCOLS,
-  PORTS,
   MESSAGES,
   REQUEST_CONSTANTS,
-  STREAMING_EVENTS,
   MODEL_FILE_COMMANDS,
   OLLAMA_LOCAL_URL,
   SHA256,
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,6 @@
 import { version } from './version.js'
 import type { Fetch, ErrorResponse } from './interfaces.js'
-import {
-  EMPTY_STRING,
-  ENCODING,
-  MESSAGES,
-  OLLAMA_LOCAL_URL,
-  PORTS,
-  PROTOCOLS,
-} from './constants'
-import { promises } from 'fs'
+import { EMPTY_STRING, ENCODING, MESSAGES, OLLAMA_LOCAL_URL } from './constants'
 
 /**
  * An error class for response errors.
@@ -255,7 +247,7 @@ export const formatHost = (host: string): string => {
       port = '11434'
     } else {
       // Assign default ports based on the protocol
-      port = url.protocol === `${PROTOCOLS.HTTPS}:` ? PORTS.HTTPS : PORTS.HTTP
+      port = url.protocol === 'https:' ? '443' : '80'
     }
   }
 
@@ -266,32 +258,4 @@ export const formatHost = (host: string): string => {
   }
 
   return formattedHost
-}
-
-/**
- * Checks if a path is a file path
- * @param path {string} - The path to check
- * @returns {Promise<boolean>} - Whether the path is a file path or not
- */
-export async function isFilePath(path: string): Promise<boolean> {
-  try {
-    await promises.access(path)
-    return true
-  } catch {
-    return false
-  }
-}
-/**
- * checks if a file exists
- * @param path {string} - The path to the file
- * @private @internal
- * @returns {Promise<boolean>} - Whether the file exists or not
- */
-export async function fileExists(path: string): Promise<boolean> {
-  try {
-    await promises.access(path)
-    return true
-  } catch {
-    return false
-  }
 }
