fe9ab424bbc43269a26b5341637f6a5b011a5bf4
Bruce MacDonald <brucewmacdonald@gmail.com>
2024-07-16 13:53:35 -0700

    add insert support to generate endpoint

--- examples/fill-in-middle/fill.ts
+++ examples/fill-in-middle/fill.ts
@@ -1,20 +1,8 @@
 import ollama from 'ollama'
 
-const prefix = `def remove_non_ascii(s: str) -> str:
-"""
-`
-const suffix = `
-return result
-`
 const response = await ollama.generate({
-  model: 'codellama:7b-code',
-  prompt: `<PRE> ${prefix} <SUF>${suffix} <MID>`,
-  options: {
-    num_predict: 128,
-    temperature: 0,
-    top_p: 0.9,
-    presence_penalty: 0,
-    stop: ['<EOT>'],
-  },
+  model: 'deepseek-coder-v2',
+  prompt: `def add(`,
+  suffix: `return c`,
 })
 console.log(response.response)
