1b4319c4c416355d541602d4d5d144eba6de193a
Craig R. Hughes <craig.git@rungie.com>
2024-10-22 15:20:04 -0700

    Add reranking

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -159,6 +159,13 @@ export interface EmbeddingsRequest {
   options?: Partial<Options>
 }
 
+export interface RerankRequest {
+  model: string
+  query: string
+  documents: string[]
+  top_n?: number
+}
+
 // response types
 
 export interface GenerateResponse {
@@ -197,6 +204,17 @@ export interface EmbedResponse {
 
 export interface EmbeddingsResponse {
   embedding: number[]
+}
+
+export interface RerankResponse {
+  model: string
+  results: {
+    document: string
+    relevance_score: number
+  }[]
+  total_duration: number
+  load_duration: number
+  prompt_eval_count: number
 }
 
 export interface ProgressResponse {
