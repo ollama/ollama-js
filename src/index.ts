343efe347b87e973f90bcefc1fca04e14ec38382
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-01-03 17:08:32 +0300

    Export `AbortableAsyncIterator` class as type

--- src/index.ts
+++ src/index.ts
@@ -173,3 +173,5 @@ export default new Ollama()
 
 // export all types from the main entry point so that packages importing types dont need to specify paths
 export * from './interfaces.js'
+
+export type { AbortableAsyncIterator }
