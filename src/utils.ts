ff96b385792f5dc5a6720499ba4af3bdac2afa5c
Yoann Moinet <597828+yoannmoinet@users.noreply.github.com>
2025-02-20 22:03:57 +0100

    utils: fallback for deprecated `window.navigator.platform` (#208)

--- src/utils.ts
+++ src/utils.ts
@@ -93,7 +93,9 @@ const checkOk = async (response: Response): Promise<void> => {
  */
 function getPlatform(): string {
   if (typeof window !== 'undefined' && window.navigator) {
-    return `${window.navigator.platform.toLowerCase()} Browser/${navigator.userAgent};`
+const platform = 
+      window.navigator.platform || window.navigator.userAgentData?.platform || 'unknown'
+    return `${platform.toLowerCase()} Browser/${navigator.userAgent};`
   } else if (typeof process !== 'undefined') {
     return `${process.arch} ${process.platform} Node.js/${process.version}`
   }
